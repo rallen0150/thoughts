ee741771e704ec30a467da251b4a9d121b6559f2
rallen0150 <rallen0150@gmail.com>
2017-05-12 13:59:26 -0400

    worked on variables in url

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^new_user/$', UserCreateView.as_view(), name='user_create_view'),
     url(r'^category/create/$', CategoryCreateView.as_view(), name='category_create_view'),
     url(r'^profile/create/$', ProfileCreateView.as_view(), name='profile_create_view'),
-    url(r'^profile/update/$', ProfileUpdateView.as_view(), name='profile_update_view'),
+    url(r'^profile/update/(?P<user>[-\w]+)/(?P<pk>\d+)/$', ProfileUpdateView.as_view(), name='profile_update_view'),
 ]
--- thoughts/models.py
+++ thoughts/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Profile(models.Model):
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=50, blank=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
     avatar = models.FileField(null=True, blank=True)
     user = models.OneToOneField('auth.User')
 
